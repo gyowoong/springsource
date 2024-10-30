bbc59325251f2ba69bf7590d43488693cd8fe24e
이교웅 <landjjang1212@gmail.com>
2024-10-30 17:34:39 +0900

    project java todo

---
+++ project2/src/test/java/com/example/project2/repository/BoardRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.example.project2.repository;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.project2.entity.Board;
+
+@SpringBootTest
+public class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void insertTest() {
+        IntStream.rangeClosed(1, 20)
+                .forEach(i -> {
+                    Board board = Board.builder()
+                            .title("자유게시판" + i)
+                            .content("abcdefg" + i)
+                            .writer("홍길동" + i)
+                            .build();
+                    boardRepository.save(board);
+                });
+    }
+
+    @Test
+    public void selectOneTest() {
+        System.out.println(boardRepository.findById(1L).get());
+    }
+
+    @Test
+    public void selectAllTest() {
+        boardRepository.findAll().forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void UpdateTest() {
+        Board board = boardRepository.findById(7L).get();
+        board.setTitle("게시판");
+        board.setContent("123456");
+        boardRepository.save(board);
+    }
+
+    @Test
+    public void deleteTest() {
+        boardRepository.deleteById(6L);
+    }
+
+}
